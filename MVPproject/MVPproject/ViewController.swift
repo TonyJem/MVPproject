dbe783df30c728e2388bbb0b798c98e5d3a1c93d
TonyJem <a.jemanov@gmail.com>
2021-02-21 21:19:13 +0200

    Create IBOutlets and IBAction for UI elements in Main.storyboard

--- MVPproject/MVPproject/ViewController.swift
+++ MVPproject/MVPproject/ViewController.swift
@@ -1,9 +1,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ourButton: UIButton!
+    @IBOutlet weak var simpleLabel: UILabel!
+    
+    lazy var presenter = Presenter()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    @IBAction func ourButtonAction(_ sender: UIButton) {
+        presenter.buttonTapped()
+    }
 }
