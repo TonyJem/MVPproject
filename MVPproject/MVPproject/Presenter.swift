b245a5b00d9cdf5468399af7cb350460eac86d81
TonyJem <a.jemanov@gmail.com>
2021-02-21 21:23:53 +0200

    Create protocol "PresentView" and update "Presenter" class

--- MVPproject/MVPproject/Presenter.swift
+++ MVPproject/MVPproject/Presenter.swift
@@ -1,9 +1,23 @@
 import Foundation
 
+protocol PresentView {
+    func updateLabel()
+}
+
 class Presenter {
+    var ourView: PresentView?
+    
     var timesTapped = 0
+    
+    init(ourView: PresentView) {
+        self.ourView = ourView
+    }
     
     func buttonTapped() {
         timesTapped += 1
+        
+        if timesTapped >= 3 {
+            self.ourView?.updateLabel()
+        }
     }
 }
